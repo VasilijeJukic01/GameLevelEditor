9fcb65790998d462f0add585ae8b5db5c78ac55e
Vasilije Jukic <vasilijejukic@gmail.com>
2025-07-04 13:22:06 +0200

    Fix EditState different layer selection

--- src/main/java/editor/state/states/EditState.java
+++ src/main/java/editor/state/states/EditState.java
@@ -8,30 +8,52 @@ import editor.model.repository.components.Tile;
 import editor.settings.SettingsKey;
 import editor.state.State;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static editor.constants.Constants.TILE_SIZE;
 
 public class EditState implements State<TabView> {
 
+    private int lastClickX = -1, lastClickY = -1;
+    private int cycleIndex = 0;
+    private final List<Tile> tilesAtLastLocation = new ArrayList<>();
+
     @Override
     public void clickPerform(int x, int y, TabView tabView) {
         int tileX = x / TILE_SIZE;
         int tileY = y / TILE_SIZE;
-        for (Node child : tabView.getLevel().getChildren()) {
-            Tile t = (Tile) child;
-            if (t.getX() == tileX && t.getY() == tileY) {
-                List<Tile> selection = (List<Tile>) tabView.getSettings().getParameter(SettingsKey.EDIT_SELECTION);
-                selection.clear();
-                selection.add(t);
-                tabView.getLevel().notify(selection);
-                EditDialog editDialog = new EditDialog(EditorFrame.getInstance(), true, t);
-                editDialog.setVisible(true);
-                selection.clear();
-                tabView.getLevel().notify(selection);
-                return;
+
+        if (tileX != lastClickX || tileY != lastClickY) {
+            lastClickX = tileX;
+            lastClickY = tileY;
+            cycleIndex = 0;
+
+            tilesAtLastLocation.clear();
+            for (Node child : tabView.getLevel().getChildren()) {
+                Tile t = (Tile) child;
+                if (t.getX() == tileX && t.getY() == tileY) tilesAtLastLocation.add(t);
             }
         }
+        else cycleIndex++;
+
+        if (tilesAtLastLocation.isEmpty()) {
+            lastClickX = -1;
+            lastClickY = -1;
+            return;
+        }
+
+        int effectiveIndex = cycleIndex % tilesAtLastLocation.size();
+        Tile tileToEdit = tilesAtLastLocation.get(effectiveIndex);
+        List<Tile> selection = (List<Tile>) tabView.getSettings().getParameter(SettingsKey.EDIT_SELECTION);
+
+        selection.clear();
+        selection.add(tileToEdit);
+        tabView.getLevel().notify(selection);
+        EditDialog editDialog = new EditDialog(EditorFrame.getInstance(), true, tileToEdit);
+        editDialog.setVisible(true);
+        selection.clear();
+        tabView.getLevel().notify(selection);
     }
 
     @Override
